5e84ec2fd3b019c517f0e4040402f488c13c74be
Carla Drieli de Oliveira Melo <carla.melo.ter@estacio.br>
2023-11-29 18:57:11 -0300

    transacoes

--- src/components/nova-transacao.component.ts
+++ src/components/nova-transacao.component.ts
@@ -2,6 +2,7 @@ import { Transacao } from "../types/Transacao.js";
 import { TipoTransacao } from "../types/TipoTransacao.js";
 import Conta from "../types/Conta.js";
 import SaldoComponent from "./saldo-component.js";
+import ExtratoComponent from "./extrato-component.js";
 
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 
@@ -19,8 +20,7 @@ elementoFormulario.addEventListener("submit", function(event){
 
         let tipoTransacao : TipoTransacao = inputTipoTransacao.value as TipoTransacao;
         let valor : number = inputValor.valueAsNumber;
-        let data: Date = new Date(inputData.value);
-        let saldo: number = Conta.getSaldo();
+        let data: Date = new Date(inputData.value + " 00:00:00");      
 
         const novaTransacao: Transacao = {
             tipoTransacao: tipoTransacao,
@@ -30,7 +30,7 @@ elementoFormulario.addEventListener("submit", function(event){
 
         Conta.registrarTransacao(novaTransacao);
         SaldoComponent.atualizar();
-
+        ExtratoComponent.atualizar();
         elementoFormulario.reset();
     }
     catch(erro){
